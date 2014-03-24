61256f6f2d8e750742ffc585bcf4c721de3bb085
ehsueh <ehsueh1993@gmail.com>
2014-03-23 20:17:32 -0400

    added matlab script

---
+++ shmoos.m
@@ -0,0 +1,41 @@
+D_2 = 0.03 
+D_3 = 11 
+nex_gef = 0.2 
+nex_intr = 0.12 
+hyd_42 = 1 
+att_42bc = 0.266 
+att_42 = 0.28 
+ext_42 = 1 
+att_b = 0.2667 
+det_b = 0.35
+att_24 = 0.00297 
+det_24 = 0.35
+R_ = 3.95 
+N_42 = 3000
+N_B = 6500
+N_24 = 1000
+
+
+function delta_mt = detlta_mt(md, mt, mbg) 
+delta_mt = 
+
+
+function delta_md = delta_md(mbg, md, mt, cd)
+delta_md = 
+
+function delta_mb = delta_mb(mt, mb, mb, cg)
+delta_mb =
+
+function delta_bg = delta_bg(mb, cg, mbg)
+delta_bg = 
+
+function delta_cg = delta_cg(cd)
+delta_cg = 
+
+function delta_cb = delta_cb(cb)
+delta_cb = 
+
+function delta_cg = delta_cg(cg)
+delta_cg =
+
+%boundary conditions
