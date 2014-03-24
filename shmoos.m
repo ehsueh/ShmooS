a75bb913aa36c74ebb195cd2352a49da6d1e7b09
teheavy <fredchen666@gmail.com>
2014-03-24 00:04:37 -0400

    Update shmoos.m

--- shmoos.m
+++ shmoos.m
@@ -17,37 +17,25 @@ N_24 = 1000
 
 
 function delta_mt = detlta_mt(md, mt, mbg) 
-delta_mt = 
+delta_mt = (mbg*nex_gef + nex_intr)*md - hyd_42*mt + att_42bc*mbg*cd + D_2*laplacian(mt, [R_, thet, phi], %)
 
 
 function delta_md = delta_md(mbg, md, mt, cd)
-delta_md = 
+delta_md = -(mbg*nex_gef + nex_intr)*md + hyd_42*mt + att_42*cd - ext_42*md + D_2*laplacian(md, [R_, thet, phi], %)
 
 function delta_mb = delta_mb(mt, mb, mb, cg)
-delta_mb =
+delta_mb = att_b*mt*cb - det_b*mb*cg - att_24*mb*cb + det_24*mbg + D_2*laplacian(mb, [R_, thet, phi], %)
 
 function delta_bg = delta_bg(mb, cg, mbg)
-delta_bg = 
+delta_bg = att_24*mb*cb - det_24 - det_24*mbg + D_2*laplacian(mbg, [R_, thet, phi], %)
 
 function delta_cg = delta_cg(cd)
-delta_cg = D_3 * cd
+delta_cg = 
 
 function delta_cb = delta_cb(cb)
-delta_cb = D_3 * cb
+delta_cb = 
 
 function delta_cg = delta_cg(cg)
-delta_cg = D_3 * cg
+delta_cg =
 
 %boundary conditions
-
-function bound_cd = bound_cd(cd, mbg, md)
-bound_cd = (-(att_42bc * mbg + att_42) * cd + ext_42 * md) / D_3
-
-function bound_cb = bound_cb(mt, cb, mb)
-bound_cb = (-(att_b * mt* cb) + det_b * mb) / D_3
-
-function bound_cg = bound_cg(mb, cg, mbg) 
-bound_cg = (-(att_24 * mb * cg) + det_24 * mbg) / D_3
-
-
-
