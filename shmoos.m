0c2b5719bbfc3681b07b4d096f8927b24f88bea2
teheavy <fredchen666@gmail.com>
2014-03-24 00:13:25 -0400

    Update shmoos.m

--- shmoos.m
+++ shmoos.m
@@ -16,25 +16,25 @@ N_B = 6500
 N_24 = 1000
 
 
-function delta_mt = delta_mt(md, mt, mbg) 
+function delta_mt = delta_mt(md, mt, mbg, thet, phi) 
 delta_mt = (mbg*nex_gef + nex_intr)*md - hyd_42*mt + att_42bc*mbg*cd + D_2*laplacian(mt, [R_, thet, phi], %)
 
-function delta_md = delta_md(mbg, md, mt, cd)
+function delta_md = delta_md(mbg, md, mt, cd, thet, phi)
 delta_md = -(mbg*nex_gef + nex_intr)*md + hyd_42*mt + att_42*cd - ext_42*md + D_2*laplacian(md, [R_, thet, phi], %)
 
-function delta_mb = delta_mb(mt, mb, mb, cg)
+function delta_mb = delta_mb(mt, mb, mb, cg, thet, phi)
 delta_mb = att_b*mt*cb - det_b*mb*cg - att_24*mb*cb + det_24*mbg + D_2*laplacian(mb, [R_, thet, phi], %)
 
-function delta_bg = delta_bg(mb, cg, mbg)
+function delta_bg = delta_bg(mb, cg, mbg, thet, phi)
 delta_bg = att_24*mb*cb - det_24 - det_24*mbg + D_2*laplacian(mbg, [R_, thet, phi], %)
 
-function delta_cg = delta_cg(cd) 
+function delta_cg = delta_cg(cd, r, thet, phi) 
 delta_cg = D_3 * laplacian(cd, [r, thet, phi], %)
 
-function delta_cb = delta_cb(cb) 
+function delta_cb = delta_cb(cb, r, thet, phi) 
 delta_cb = D_3 * laplacian(cb, [r, thet, phi], %)
 
-function delta_cg = delta_cg(cg) 
+function delta_cg = delta_cg(cg, r, thet, phi) 
 delta_cg = D_3 * laplacian(cg, [r, thet, phi], %)
 
 %boundary conditions 
